cabde334f0b0852a7c038c9c1c1893bea2d29274
/\///\/_//\ _ <tojicodes@gmail.com>
2023-08-03 21:42:43 -0500

    added js files

---
+++ redux/reducers/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    // define initial state here
+};
+
+function yourReducer(state = initialState, action) {
+    switch(action.type) {
+      // handle different action types here
+    default:
+        return state;
+    }
+}
+
+export default yourReducer;
